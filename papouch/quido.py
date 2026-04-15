e82049c7a53dad935f9431f42c8265204de2a808
Libor Wagner <libor.wagner@cvut.cz>
2026-04-15 15:50:31 +0200

    WIP: improving error handling and typing

--- papouch/quido.py
+++ papouch/quido.py
@@ -1,20 +1,21 @@
+from dataclasses import dataclass
 
 import logging
-from typing import Any
+from typing import Any, List, Optional
 import requests
 import socket
 import serial
 
+from papouch.error import QuidoError
+
 log = logging.getLogger()
 
-class PapouchError(Exception):
 
-    def __init__(self, msg='', recv=None):
-        self.msg = msg
-        self.recv = recv
+@dataclass
+class QuidoInfo:
+    name: str
 
-    def __str__(self):
-        return repr(self.msg + ": " + self.recv if self.recv else "")
+# b'0\r*B10Quido ETH 4/4; v0254.03.35; f66 97; t1\r'
 
 
 class Quido(object):
@@ -33,7 +34,7 @@ class Quido(object):
         self.cmd(b'IS', b'0') # disable sending changes
         log.debug("Connected to tcp ip: %s port: %s", self.tcp_ip, self.tcp_port)
 
-    def __cmd_tcp(self, inst, data=b'', adr=b'$', buff=1000):
+    def __cmd_tcp(self, inst, data=b'', adr=b'$', buff=1000) -> bytes:
         msg = b'*B' + adr + inst + data + b'\r'
         self.socket.send(msg)
         log.debug("Cmd send: %s", msg)
@@ -41,7 +42,7 @@ class Quido(object):
         log.debug("Cmd recv: %s", recv)
         return recv
 
-    def __recv_tcp(self, buff=1000):
+    def __recv_tcp(self, buff=1000) -> bytes:
         recv = self.socket.recv(buff)
         log.debug("Cmd recv: %s", recv)
         return recv
@@ -55,7 +56,7 @@ class Quido(object):
         self.recv = self.__recv_serial
         log.debug("Connected to serial dev: %s baud: %d", self.dev, self.baud)
 
-    def __cmd_serial(self, inst, data=b'', adr=b'$', buff=1000):
+    def __cmd_serial(self, inst, data=b'', adr=b'$', buff=1000) -> bytes:
         msg = b'*B' + adr + inst + data + b'\r'
         self.ser.write(msg)
         self.ser.flush()
@@ -63,13 +64,13 @@ class Quido(object):
         recv = self.__recv_serial()
         return recv
 
-    def __recv_serial(self, buff=None):
+    def __recv_serial(self, buff=None) -> bytes:
         recv = read_util(self.ser)
         log.debug("Serial read: %s", recv)
         return recv
 
-    def check_reponse(self, resp):
-        return len(resp) > 3 and resp[3] == '0'
+    def check_reponse(self, resp) -> bool:
+        return len(resp) > 3 and resp[3:4] == b'0'
 
     def reset(self):
         inst = b'RE'
@@ -81,15 +82,16 @@ class Quido(object):
             log.error("Unable reset, response: %s", recv)
             return None
 
-    def get_name(self):
+    def get_name(self) -> List[str]:
         inst = b'?'
         recv = self.cmd(inst)
 
         if self.check_reponse(recv):
-            return [v.strip() for v in recv[4:-1].split(';')]
+            text = recv[4:-1].decode('utf-8')
+            return [v.strip() for v in text.split(';')]
         else:
             log.error("Unable to read name, response: %s", recv)
-            return None
+            raise QuidoError("Unable to read name", recv)
 
     def get_temperature(self, n=1):
         inst = b'TR'
@@ -168,14 +170,14 @@ def get_val_is(recv, n):
     return True if recv[n-1] == 'H' else False
 
 
-def read_util(ser, eol=b'\r'):
-    buffer = ""
+def read_util(ser, eol:bytes = b'\r') -> bytes:
+    buffer = bytearray()
     while True:
-        oneByte = ser.read(1)
-        if oneByte == eol:
-            return buffer
+        one_byte = ser.read(1)
+        if one_byte == eol:
+            return bytes(buffer)
         else:
-            buffer += oneByte.decode("ascii")
+            buffer.extend(one_byte)
 
 
 def as_bytes(val: Any):
